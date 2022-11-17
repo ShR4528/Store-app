9635abd4fbb9f6f6009d1ed6d54b9d210ee31b7a
Shr4528 <Shr4528@gmail.com>
2022-11-17 19:46:43 +0200

    commit

---
+++ app/src/components/Product.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { IProduct } from '../models';
+
+
+
+interface ProductProps {
+    product: IProduct
+}
+export function Product({ product }: ProductProps ) {
+    const [details, setDetails] = useState(false)
+
+    const btnBgClassName = details ? 'bg-yellow' : 'bg-blue-400'
+
+    const btnClasses = ['py-2 px-4 border' ,btnBgClassName] 
+
+    return (
+    <div  className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
+        <img src={ product.image} className="w-1/6" alt={product.title} />
+        <p>{ product.title }</p>
+        <p className='font-bold'>{product.price}</p>
+
+        <button 
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails(prev => !prev)}
+        >
+       { details ? 'Hide Details' : 'Show Details' }
+        </button>
+
+        {/* <button 
+        className='py-2 px-4 border bg-blue-400'
+        onClick={() => setDetails(false)}
+        >
+        Hide Details
+        </button> */}
+
+       {details && <div>
+           <p>{ product.description }</p>
+        </div>}
+
+    </div>
+    )
+}
